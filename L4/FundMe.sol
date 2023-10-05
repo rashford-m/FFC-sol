2f8813e7539499f5154aca2b9c68b8ef644ae532
Rashford <mikearowo@gmail.com>
2023-10-05 13:17:35 +0100

    new

--- L4/FundMe.sol
+++ L4/FundMe.sol
@@ -3,5 +3,10 @@
 pragma solidity ^0.8.8;
 
 contract FundMe {
-    
+    uint256 public minimumUsd = 50;
+
+    function fund() public payable {
+        // want to be able to set a minimum fund amount in USD
+        // 1. How do we send ETH to this contract
+    }
 }
